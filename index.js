a90c1a7c080ecb606d2db1b6aa84ead81af238b7
Ivan <ivanalexispereyra97@gmail.com>
2023-09-03 20:20:10 -0300

    end

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 function parImpar(i) {
     if(i % 2 === 0) {
-        console.log("El numero es par")
+        console.log("El numero es par");
     }
     else{
-        console.log("El numero es impar")
+        console.log("El numero es impar");
     }
 }
 
@@ -15,13 +15,13 @@ parImpar(6)
 
 function mayorMenor(a, b) {
     if(a > b) {
-        console.log(a, " es mayor que ", b)
+        console.log(a, " es mayor que ", b);
     }
     else if(a < b) {
-        console.log(a, " es menor que ", b)
+        console.log(a, " es menor que ", b);
     }
     else {
-        console.log(a, " es igual que ", b)
+        console.log(a, " es igual que ", b);
     }
 } 
 
@@ -31,10 +31,10 @@ mayorMenor(10, 7)
 
 function multiplo(i) {
     if(i % 5 === 0) {
-        console.log("El numero es múltiplo de 5")
+        console.log("El numero es múltiplo de 5");
     }
     else{
-        console.log("El numero no es múltiplo de 5")
+        console.log("El numero no es múltiplo de 5");
     }
 }
 
@@ -52,16 +52,48 @@ contador(5)
 
 // 5)
 
+function repetir(a, b) {
+    for( let cont = 1; cont <= a; cont++) {
+        console.log(b);
+    }
+}
 
+repetir(4, "verde")
 
 // 6)
 
+const frutas = ["Pera", "Naranja", "banana", "melon"]
 
+function tipoFrutas(arr) {
+    for( let i = 0; i < arr.length; i++) {
+        console.log(arr[i])
+    }
+}
+
+tipoFrutas(frutas)
 
 // 7)
 
+const numeros = [5, 9, 15, 1, 43, 20, 96, 71, 26, 11]
 
+function menosQuinto(num) {
+    for( let i = 0; i < num.length; i++) {
+        if(i != 4 ){
+            console.log(num[i])
+        }
+    }
+}
+
+menosQuinto(numeros)
 
 //8)
 
+const multi = [10, 6, 9, 2]
 
+function numeroMulti(x, y) {
+    for( let i = 0; i < x.length; i++) {
+        console.log(x[i] * y)
+    }
+}
+
+numeroMulti(multi, 10)
