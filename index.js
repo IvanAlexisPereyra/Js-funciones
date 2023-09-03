92b089f413c09ad9c2f52da46a5fe09b792ed401
Ivan <ivanalexispereyra97@gmail.com>
2023-09-03 19:03:46 -0300

    mid

--- index.js
+++ index.js
@@ -1,11 +1,67 @@
+// 1)
+
 function parImpar(i) {
     if(i % 2 === 0) {
-        console.log('Es par')
+        console.log("El numero es par")
     }
     else{
-        console.log('Es impar')
+        console.log("El numero es impar")
     }
 }
 
-parImpar(4)
+parImpar(6)
+
+// 2)
+
+function mayorMenor(a, b) {
+    if(a > b) {
+        console.log(a, " es mayor que ", b)
+    }
+    else if(a < b) {
+        console.log(a, " es menor que ", b)
+    }
+    else {
+        console.log(a, " es igual que ", b)
+    }
+} 
+
+mayorMenor(10, 7)
+
+// 3)
+
+function multiplo(i) {
+    if(i % 5 === 0) {
+        console.log("El numero es múltiplo de 5")
+    }
+    else{
+        console.log("El numero no es múltiplo de 5")
+    }
+}
+
+multiplo(25)
+
+// 4)
+
+function contador(i) {
+    for(let cont = 0; cont <= i; cont++) {
+        console.log(cont);
+    }
+}
+
+contador(5)
+
+// 5)
+
+
+
+// 6)
+
+
+
+// 7)
+
+
+
+//8)
+
 
